9bdfdb79bde40d72bcfbc1be5d94ccc765d191e1
makeabignews <makeabignews@sina.com>
2017-11-01 16:25:48 +0800

    Update udpserver.py

--- udpserver.py
+++ udpserver.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 # -*- coding:UTF-8 -*-
-from socket import *  
-from time import ctime  
-  
-HOST = ''  
-PORT = 8888  
-BUFSIZ = 128  
-ADDR = (HOST, PORT)  
-  
-#创建一个服务器端UDP套接字  
-udpServer = socket(AF_INET, SOCK_DGRAM)  
-#绑定服务器套接字  
-udpServer.bind(ADDR)  
-  
-while True:  
-    print 'waiting for message...'  
-#接收来自客户端的数据  
-    data, addr = udpServer.recvfrom(BUFSIZ)  
+from socket import *
+from time import ctime
+from os import *
+HOST = ''
+PORT = 8888
+BUFSIZ = 128
+ADDR = (HOST, PORT)
+udpServer = socket(AF_INET, SOCK_DGRAM)
+udpServer.bind(ADDR)
+while True:
+    print 'waiting for message...'
+    data, addr = udpServer.recvfrom(BUFSIZ)
     print 'get msg:',data
-#向客户端发送数据  
-    udpServer.sendto('[%s] %s' % (ctime(), data), addr)  
-    print '...received from and returned to:', addr  
-udpServer.close()  
+    power_command=data.split('_')[0]
+    pin_number=data.split('_')[1]
+    if(power_command=='on'):
+      print 'on:',pin_number
+      system('sudo ./power.sh %s %s' % (pin_number,1))
+    if(power_command=='off'):
+      print 'off:',pin_number
+      system('sudo ./power.sh %s %s' % (pin_number,0))
+    udpServer.sendto('[%s] %s' % (ctime(), data), addr)
+    print '...received from and returned to:', addr
+udpServer.close()
